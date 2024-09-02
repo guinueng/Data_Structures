51ad9e127b287894f155fdb5453ff8f0e1ae5846
Guinueng <admin@guinueng.com>
2024-09-02 16:38:51 +0900

    Tested something.

--- Assignment_1/Task_1.cc
+++ Assignment_1/Task_1.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     cout << "Hello world!" << endl;
-    cout << "My name is [20211216] Uk Lee." << endl;
+    cout << "My name is [20211216] GwanUk Lee." << endl;
 
     return 0;
 }
