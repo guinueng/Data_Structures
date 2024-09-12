6bfe30b382d28828abb3410e74ea932d3262197e
Guinueng <admin@guinueng.com>
2024-09-12 16:28:34 +0900

    Added Device class function codes.

--- Assignment_2/device.cpp
+++ Assignment_2/device.cpp
@@ -2,3 +2,41 @@
 
 /* Write your code here */
 
+Device::Device(int id, const std::string& type, const std::string& brand)
+    :device_id(id), device_type(type), brand(brand) {}
+
+bool Device::operator==(const Device& other) const{ // Compare all of component in device class.
+    if(this -> device_id == other.device_id) // Case 1. device_id, type, and brand are all same.
+        if(this -> device_type.compare(other.device_type) == 0)
+            if(this -> brand.compare(other.brand) == 0) // Reference : https://cplusplus.com/reference/string/string/compare/
+                return true;
+    // Case 2. If one of them are different.
+    return false;
+}
+bool Device::operator<(const Device& other) const{ // Operator <, >, <=, >= compares only device id.
+    if(this -> device_id < other.device_id)
+        return true;
+    else
+        return false;
+}
+
+bool Device::operator>(const Device& other) const{
+    if(this -> device_id > other.device_id)
+        return true;
+    else
+        return false;
+}
+
+bool Device::operator<=(const Device& other) const{
+    if(this -> device_id <= other.device_id)
+        return true;
+    else
+        return false;
+}
+
+bool Device::operator>=(const Device& other) const{
+    if(this -> device_id >= other.device_id)
+        return true;
+    else
+        return false;
+}
